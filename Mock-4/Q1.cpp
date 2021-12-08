ff22b3b549f62ace7a7899206758e1536c428e12
Megha Sahni <42293547+msahni825@users.noreply.github.com>
2021-12-08 20:11:36 +0530

    Update Q1.cpp

--- Mock-4/Q1.cpp
+++ Mock-4/Q1.cpp
@@ -6,35 +6,38 @@ using namespace std;
   int row_coordinates[4]={1,0,-1,0};
   int col_coordinates[4]={0,-1,0,1};
  
-bool isValid(vector<vector<int>>& binaryMatrix, int i, int j){
-  if(i<0 || j<0 || i>binaryMatrix.size() || j>binaryMatrix[0].size()){
+bool isValid(vector<vector<int>>& grid, int i, int j){
+  if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()){
     return false;
   }
   return true;
 }
-void dfs(vector<vector<int>>& binaryMatrix, int row, int col){
-  for(int i=0;i<4;i++){// row=0, col=1
-    int x=row_coordinates[i]+row;//1+0=1
-    int y=col_coordinates[i]+col;//0+1=1
-    if(!isValid(binaryMatrix,x,y)){//i=1,j=1
+
+void dfs(vector<vector<int>>& grid, int row, int col){
+  grid[row][col]=0;
+  for(int k=0;k<4;k++){// row=0, col=1
+    int x=row_coordinates[k]+row;//1+0=1
+    int y=col_coordinates[k]+col;//0+1=1
+    if(!isValid(grid,x,y)){//i=1,j=1
        continue;
     }
-    else if(isValid(binaryMatrix,x,y) && binaryMatrix[x][y]=='1'){
-      dfs(binaryMatrix,x,y);
+    if(grid[x][y]==1){
+      dfs(grid,x,y);
     }
   }
 }
-int getNumberOfIslands(vector<vector<int>>& binaryMatrix) 
+int getNumberOfIslands(const vector<vector<int>>& binaryMatrix) 
 {
   // your code goes here
-  int row=binaryMatrix.size();
-  int col=binaryMatrix[0].size();
+  vector<vector<int>> grid = binaryMatrix;
+  int row=grid.size();
+  int col=grid[0].size();
   int isl_count=0;
   for(int i=0;i<row;i++){
     for(int j=0;j<col;j++){
-      if(binaryMatrix[i][j]=='1'){
+      if(grid[i][j]==1){
          isl_count++;//i=0, j=1, count=1
-         dfs(binaryMatrix,i,j);// mat,0,1
+         dfs(grid,i,j);// mat,0,1
       }
     }
   }
